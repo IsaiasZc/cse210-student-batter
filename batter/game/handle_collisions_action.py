dc98e54b97ab0e449a301f72a5452797ab3b518a
IsaiasZc <78005126+IsaiasZc@users.noreply.github.com>
2021-11-03 21:09:06 -0500

    Co-authored-by: LFernandoPortugal <LFernandoPortugal@users.noreply.github.com>

---
+++ batter/game/handle_collisions_action.py
@@ -0,0 +1,34 @@
+import random
+from game import constants
+from game.action import Action
+
+class HandleCollisionsAction(Action):
+    """A code template for handling collisions. The responsibility of this class of objects is to update the game state when actors collide.
+    
+    Stereotype:
+        Controller
+    """
+
+    def execute(self, cast):
+        """Executes the action using the given actors.
+
+        Args:
+            cast (dict): The game actors {key: tag, value: list}.
+        """
+        paddle = cast["paddle"][0] # there's only one
+        ball = cast["ball"][0] # there's only one
+        bricks = cast["brick"]
+        for brick in bricks:
+            if ball.get_position().equals(brick.get_position()):
+                bricks.pop(brick)
+                ball.reverse()
+
+
+    def wall_collision():
+        pass
+        """change the direcction for the x axis without moving the y axis"""
+
+
+    def paddle_brick_collision():
+        pass
+        """change the direcction for the y axis without moving the x axis"""
