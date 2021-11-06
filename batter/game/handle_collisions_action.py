307ad5098b3e6181702135ef3e1894ec3415b01b
IsaiasZc <78005126+IsaiasZc@users.noreply.github.com>
2021-11-06 17:20:13 -0500

    epaaa
    Co-authored-by: LFernandoPortugal <LFernandoPortugal@users.noreply.github.com>

--- batter/game/handle_collisions_action.py
+++ batter/game/handle_collisions_action.py
@@ -1,6 +1,7 @@
 import random
 from game import constants
 from game.action import Action
+from game.point import Point
 
 class HandleCollisionsAction(Action):
     """A code template for handling collisions. The responsibility of this class of objects is to update the game state when actors collide.
@@ -18,18 +19,12 @@ class HandleCollisionsAction(Action):
         paddle = cast["paddle"][0] # there's only one
         ball = cast["ball"][0] # there's only one
         bricks = cast["brick"]
-        for brick in bricks:
+        for idx,brick in enumerate(bricks):
             if ball.get_position().equals(brick.get_position()):
-                position = brick.get_position()
-                
-                # ball.reverse()
-
-
-    def wall_collision():
-        pass
-        """change the direcction for the x axis without moving the y axis"""
-
-
-    def paddle_brick_collision():
-        pass
-        """change the direcction for the y axis without moving the x axis"""
+                bricks.pop(idx)
+                velocity = ball.get_velocity().reverse_y()
+                ball.set_velocity(velocity)
+            if ball.get_position().equals(brick.get_position()):
+                bricks.pop(idx)
+                velocity = ball.get_velocity().reverse_x()
+                ball.set_velocity(velocity)
