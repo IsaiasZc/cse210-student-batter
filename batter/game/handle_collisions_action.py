de85e78a23ef458ad094c9d570e8cef181f3ba1b
IsaiasZc <78005126+IsaiasZc@users.noreply.github.com>
2021-11-06 17:49:46 -0500

    wii3

--- batter/game/handle_collisions_action.py
+++ batter/game/handle_collisions_action.py
@@ -22,12 +22,25 @@ class HandleCollisionsAction(Action):
         for idx,brick in enumerate(bricks):
             if ball.get_position().equals(brick.get_position()):
                 bricks.pop(idx)
-                self._brick_collision(ball)
+                self._paddle_or_brick_collision(ball)
+                
         if (ball.get_position().get_x() == constants.MAX_X) or (ball.get_position().get_x() == 1):
             self._wall_collision(ball)
+            
+        if paddle.get_position().equals(ball.get_position()):
+            self._paddle_or_brick_collision(ball)
+        
+        for i in range(11):
+            x = paddle.get_position().get_x() + i
+            y = paddle.get_position().get_y()
+            if ball.get_position().get_x() == x and ball.get_position().get_y() == y:
+                self._paddle_or_brick_collision(ball)
 
+    # aun tenemos un problema
+    # el cambio de direccion aun sucede despues de estar en la posicion del objeto
+    
 
-    def _brick_collision(self, ball):
+    def _paddle_or_brick_collision(self, ball):
         y = ball.get_velocity().get_y() * -1
         x = ball.get_velocity().get_x()
         ball.set_velocity(Point(x,y))    
